52b80c56bf3e8517e736116582a7afa63d715334
Megan Lee <mlee@10562-mlee.lan>
2018-11-13 13:28:19 +1100

    Create chapter10.go

---
+++ chapter10/chapter10.go
@@ -0,0 +1,15 @@
+package main
+
+/* How do you specify the direction of a channel type? */
+/* Answer: */
+
+/* Write your own Sleep function using time.After. */
+// TODO
+
+/* What is a buffered channel? How would you create
+one with a capacity of 20? */
+/* Answer: */
+
+func main() {
+
+}
